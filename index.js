5f29107e5c578fd557b77dc128dfa90dd5ea49ed
= <=>
2025-07-21 12:46:19 +0200

    Project structure

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error(err));
+
+app.use('/api', require('./routes/protectedRoutes'));
+
+// const { swaggerUi, swaggerSpec } = require('./utils/swagger');
+
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
